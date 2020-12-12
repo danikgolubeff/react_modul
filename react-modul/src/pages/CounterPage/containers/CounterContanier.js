84760a680497ecd2fdcc2b7f6e567ad63d849c0e
Danik <dan.golubev@gmail.com>
2020-12-12 13:55:30 +0300

    Counter

---
+++ react-modul/src/pages/CounterPage/containers/CounterContanier.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Counter from "../сomponents/Counter/Counter";
+
+class CounterContanier extends Сomponent {
+
+    render()    {
+        return (
+            <Counter />
+
+        );
+    }
+}
+
+export default CounterContanier;
---
+++ react-modul/src/pages/CounterPage/сomponents/Counter/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+           Counter component
+        </div>
+    );
+};
+
+export default Counter;
